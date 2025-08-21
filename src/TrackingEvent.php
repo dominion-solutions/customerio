8e3bfd9f1827cc7be128783d6b1077074e5e38c3
Mark J. Horninger <mark.horninger@dominion.solutions>
2025-08-21 12:20:19 -0400

    Added additionalFields to Tracking Event too

--- src/TrackingEvent.php
+++ src/TrackingEvent.php
@@ -10,16 +10,17 @@ class TrackingEvent implements Arrayable
         public ?string $userId = null,
         public ?string $event = null,
         public ?array $properties = [],
+        public ?array $additionalFields = [],
         public ?array $context = [],
     ) {}
 
     public function toArray(): array
     {
-        return [
+        return collect([
             'userId' => $this->userId,
             'event' => $this->event,
             'properties' => $this->properties,
             'context' => $this->context,
-        ];
+        ])->merge($this->additionalFields)->toArray();
     }
 }
--- src/User.php
+++ src/User.php
@@ -13,13 +13,13 @@ class User implements Arrayable
         public ?array $traits = [],
     ) {}
 
-    public function toArray()
+    public function toArray() : array
     {
         $baseData = collect([
             'userId' => $this->userId,
             'anonymousId' => $this->anonymousId,
             'traits' => $this->traits,
         ]);
-        return $baseData->merge($this->additionalFields);
+        return $baseData->merge($this->additionalFields)->toArray();
     }
 }
